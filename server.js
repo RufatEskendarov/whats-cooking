412fcbef94f5774c97761aaded826fa716e47307
Brian Wyatt <brianlwyatt@gmail.com>
2023-02-09 19:34:39 -0400

    get and post for userRoutes

--- server.js
+++ server.js
@@ -2,15 +2,33 @@ const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
 const routes = require("./controllers");
+
 //TODO: Uncomment to make use of database, once set up
 const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine("handlebars", exphbs.engine());
+const sess = {
+    secret: 'Super secret secret',
+    resave: false,
+    saveUninitialized: false,
+  };
+  
+app.use(session(sess));
+
+const hbs = exphbs.create();
+
+app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 //Setup routes to the Server
 //Look at /controllers folder
 app.use("/", routes);
@@ -20,8 +38,4 @@ sequelize.sync({force: false}).then(()=>{
     app.listen(PORT, () => {
         console.log(`Server is listening at http://localhost:${PORT}`)
     });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is listening at http://localhost:${PORT}`);
 });
