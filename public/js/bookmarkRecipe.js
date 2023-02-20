0d7a206dcf0ed8e92999e7cc0ebfd13b83dd91ea
Rufat Eskendarov <rufat.eskendarov@gmail.com>
2023-02-20 11:46:36 -0400

    Logic for updating Serving size

--- public/js/bookmarkRecipe.js
+++ public/js/bookmarkRecipe.js
@@ -1,29 +1,90 @@
-const bookmarkRecipe = async (event, req) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    // const userId = req.session.user_id;
-   const userId = document.querySelector('#user-info').dataset.userId;
-   console.log(userId);
-   const recipeId = 2;
-  
-    if (userId) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/userrecipes', {
-        method: 'POST',
-        body: JSON.stringify({ userId, recipeId }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the dashboard page
-        document.location.replace('/profile');
-      } else {
-        alert('Login failed, please try again!');
-      }
-    }
+const btnIncreaseServings = document.getElementById("btn--increase");
+const btnDecreaseServings = document.getElementById("btn--decrease");
+console.log(btnIncreaseServings);
+
+const updateServings = async function () {
+  const getCurrentRecipe = async function () {
+    const currentRecipeId = window.location.pathname.split("/")[2];
+
+    const curRecipe = await fetch(`/api/recipes/${currentRecipeId}`).then(
+      (res) => res.json()
+    );
+    return curRecipe;
   };
 
-  document
-  .querySelector('#bookmark-button')
-  .addEventListener('click', bookmarkRecipe);
+  let recipe = await getCurrentRecipe();
+
+  const updatedServings = async function (newServings) {
+    recipe.ingredients.forEach((ing) => {
+      ing.quantity = (ing.quantity * newServings) / recipe.servings;
+    });
+
+    recipe.servings = newServings;
+    return recipe;
+  };
+
+  btnIncreaseServings.addEventListener("click", async function (e) {
+    e.preventDefault();
+    newServings = recipe.servings + 1;
+
+    await updatedServings(newServings).then((rec) => rec);
+    const group = document.querySelectorAll(".recipe__quantity");
+
+    for (let i = 0; i < group.length; i++) {
+      if (group[i].innerHTML) {
+        group[i].innerHTML = recipe.ingredients[i].quantity.toFixed(1);
+      }
+    }
+    const servingSize = document.querySelector(".recipe__info-data--people");
+    servingSize.innerHTML = newServings;
+  });
+
+  btnDecreaseServings.addEventListener("click", async function (e) {
+    e.preventDefault();
+    newServings = recipe.servings - 1;
+
+    if (newServings <= 0) return;
+    await updatedServings(newServings).then((rec) => rec);
+    const group = document.querySelectorAll(".recipe__quantity");
+
+    for (let i = 0; i < group.length; i++) {
+      if (group[i].innerHTML) {
+        group[i].innerHTML = recipe.ingredients[i].quantity.toFixed(1);
+      }
+    }
+
+    const servingSize = document.querySelector(".recipe__info-data--people");
+    servingSize.innerHTML = newServings;
+  });
+};
+
+updateServings();
+const bookmarkRecipe = async (event, req) => {
+  event.preventDefault();
+
+  // Collect values from the login form
+  // const userId = req.session.user_id;
+  const userId = document.querySelector("#user-info").dataset.userId;
+  console.log(userId);
+  const recipeId = 2;
+
+  if (userId) {
+    // Send a POST request to the API endpoint
+    const response = await fetch("/api/userrecipes", {
+      method: "POST",
+      body: JSON.stringify({ userId, recipeId }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace("/profile");
+    } else {
+      alert("Login failed, please try again!");
+    }
+  }
+};
+
+// document
+//   .querySelector(".bookmark-button")
+//   .addEventListener("click", bookmarkRecipe);
