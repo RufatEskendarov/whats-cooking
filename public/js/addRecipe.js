434d3d6172d6482637a773cf14ebb1ddb520724c
RufatEskendarov <rufat.eskendarov@gmail.com>
2023-02-20 20:21:57 -0400

    Last version CODE

--- public/js/addRecipe.js
+++ public/js/addRecipe.js
@@ -1,46 +1,76 @@
 const displayRecipeForm = (event) => {
-    event.preventDefault();
-  
-    document.querySelector('.overlay').classList.remove('is-hidden');
-    document.querySelector('.add-recipe-window').classList.remove('is-hidden');
-  }
+  event.preventDefault();
 
-const addNewRecipe = (event) => {
+  document.querySelector(".overlay").classList.remove("is-hidden");
+  document.querySelector(".add-recipe-window").classList.remove("is-hidden");
+};
+
+const addNewRecipe = async (event) => {
   event.preventDefault();
 
   const userGenerated = true;
-  const title = document.querySelector('#recipe-title').value.trim();
-  const publisher = document.querySelector('#recipe-publisher').value.trim();
-  const sourceUrl = document.querySelector('#recipe-url').value.trim();
-  const image = document.querySelector('#recipe-image').value.trim();
-  const servings = document.querySelector('#recipe-servings').value.trim();
-  const cookingTime = document.querySelector('#recipe-cooking-time').value.trim();
+  const title = document.querySelector("#recipe-title").value.trim();
+  const publisher = document.querySelector("#recipe-publisher").value.trim();
+  const sourceUrl = document.querySelector("#recipe-url").value.trim();
+  const image = document.querySelector("#recipe-image").value.trim();
+  const servings = document.querySelector("#recipe-servings").value.trim();
+  const cookingTime = document
+    .querySelector("#recipe-cooking-time")
+    .value.trim();
 
-  console.log(title);
+  const ingredientFields = document.querySelectorAll(
+    "#ingredient-column input"
+  );
 
-  // const ingredientForm = document.querySelector('')
-  const ingredientFields = document.querySelectorAll('#ingredient-column input');
-  const ingredients = [];
+  const ingredients = Array.from(ingredientFields)
+    .map((ingredientFields) => {
+      const ingredient = ingredientFields.value.trim().split(",");
 
-  for (var i = 0; i < ingredientFields.length; i++) {
-    const ingredient = ingredientFields[i].value;
-    ingredients.push(ingredient);
-    console.log(ingredients);
+      return {
+        quantity: ingredient[0] || "",
+        unit: ingredient[1] || "",
+        description: ingredient[2] || "",
+      };
+    })
+    .filter((o) => o.quantity || o.unit || o.description);
+
+  console.log(ingredients);
+
+  const newRecipe = await fetch("/api/recipes", {
+    method: "POST",
+    body: JSON.stringify({
+      userGenerated,
+      title,
+      publisher,
+      sourceUrl,
+      image,
+      servings,
+      cookingTime,
+      ingredients,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (newRecipe.ok) {
+    document.location.reload();
+  } else {
+    alert("Unable to create new recipe.");
   }
+};
 
-  
+const closeModal = (event) => {
+  event.preventDefault();
 
-  // const newRecipe = await fetcj('/api/recipes', {
-  //   method: 'POST',
-  //   body: JSON.stringify(req.body),
-  //   headers: { 'Content-Type': 'application/json' },
-  // });
-}
-
-document
-  .querySelector('#add-recipe-button')
-  .addEventListener('click', displayRecipeForm);
+  document.querySelector(".overlay").classList.add("is-hidden");
+  document.querySelector(".add-recipe-window").classList.add("is-hidden");
+};
 
 document
-  .querySelector('.upload')
-  .addEventListener('submit', addNewRecipe);
+  .querySelector("#add-recipe-button")
+  .addEventListener("click", displayRecipeForm);
+
+document.querySelector(".upload").addEventListener("submit", addNewRecipe);
+
+document
+  .querySelector(".btn--close-modal")
+  .addEventListener("click", closeModal);
--- public/js/recipeSearch.js
+++ public/js/recipeSearch.js
@@ -1,18 +1,24 @@
 const erororM = document.querySelector(".error");
 const message = document.querySelector(".message");
+const resultsField = document.querySelector(".results");
+const searchInput = document.getElementById("search-input");
+console.log();
 
 const sideSearchListP = document.querySelector(".results");
 
 const recipeSearch = async (event) => {
   event.preventDefault();
+
+  //Clears the field of found recipes before a new search
+  resultsField.innerHTML = "";
   // Collect values from the login form
   const search = document.querySelector("#search-input").value.trim();
-  console.log(search);
 
   if (search) {
-    const response = await fetch(`/api/recipes/find?search=${search}`).then((res) =>
-      res.json()
+    const response = await fetch(`/api/recipes/find?search=${search}`).then(
+      (res) => res.json()
     );
+    console.log(response);
 
     if (response.length < 1) {
       message.classList.toggle("is-hidden");
@@ -33,10 +39,10 @@ const recipeSearch = async (event) => {
             </h4>
             <p class="preview__publisher">${res.publisher}</p>
             <div class="preview__user-generated ${
-              !res.user ? "is-hidden" : ""
+              !res.userGenerated ? "is-hidden" : ""
             }">
               <svg class="preview__icon">
-                <use href="./media/icons.svg#icon-user"></use>
+                <use href="/media/icons.svg#icon-user"></use>
               </svg>
             </div>
           </div>
@@ -47,6 +53,8 @@ const recipeSearch = async (event) => {
       );
     }
   }
+  //Clears the input field
+  searchInput.value = "";
 };
 
 document.querySelector("#search-form").addEventListener("submit", recipeSearch);
